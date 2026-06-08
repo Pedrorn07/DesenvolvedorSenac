b0549d51d38c019735071ba9b35767415aad44cf
Prenanrn <pedrorenananlmeida015@gmail.com>
2026-06-08 11:30:54 -0300

    aulanova

---
+++ UC1/AULA_05/assets/js/scripts.js
@@ -0,0 +1,26 @@
+// VARIÁVEL 
+let nome = "Pedro ";
+nome = "everybody haters " + nome;
+
+console.log("Name:" + nome)
+
+const nomeDev = "pedro"
+
+console.log("Uso constante: " + nomeDev)
+
+let nome1 = "Renan";
+let idade = 22;
+let estadocivil = "solteiro: "
+let totalfilhos = "0: "
+let naturalidade = "Fortaleza-ce: "
+let sexo = "homo sapiens: "
+
+let dados = "\nNome: " 
++ nome1 + "\nIdade: " 
++ idade + "\nEstado civil: " 
++ estadocivil + "\nFilho: " 
++ totalfilhos + "\nNaturalidade: "
++ naturalidade + "\nSexo: "
++ sexo
+
+console.log(dados)
