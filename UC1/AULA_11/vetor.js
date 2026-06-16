91bd714e1c61adf8bcc5f9c156ac05b116977522
Prenanrn <pedrorenananlmeida015@gmail.com>
2026-06-16 11:36:13 -0300

    nova aula

---
+++ UC1/AULA_11/vetor.js
@@ -0,0 +1,18 @@
+
+let nomes = [
+    "João paulo",
+    "Maria luíza",
+    "Carlos josé",
+    "Pedro lúcio"
+];
+let Tamanho = nomes.length;
+console.log(`Tamanho da array: ${Tamanho}`);
+console.log(`Exibindo dados da array`);
+console.log(`Posição 1 ${nomes[1]}`);
+
+let i = 1
+for (i; i >= 10; i++) {
+
+console.log(`${i}-${nomes[i]}`);
+
+}
